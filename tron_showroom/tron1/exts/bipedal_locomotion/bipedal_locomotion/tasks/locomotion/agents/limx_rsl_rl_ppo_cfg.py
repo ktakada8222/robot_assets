eca795068abde299df7d82dd35170604eba277a9
ktakada8222 <k.takada@gmail.com>
2026-05-11 17:20:10 +0900

    fourth commit

---
+++ tron_showroom/tron1/exts/bipedal_locomotion/bipedal_locomotion/tasks/locomotion/agents/limx_rsl_rl_ppo_cfg.py
@@ -0,0 +1,153 @@
+from isaaclab.utils import configclass
+from isaaclab_rl.rsl_rl import RslRlOnPolicyRunnerCfg, RslRlPpoActorCriticCfg, RslRlPpoAlgorithmCfg
+from bipedal_locomotion.utils.wrappers.rsl_rl.rl_mlp_cfg import EncoderCfg, RslRlPpoAlgorithmMlpCfg
+
+import os
+robot_type = os.getenv("ROBOT_TYPE")
+
+# Isaac Lab original RSL-RL configuration
+@configclass
+class PFPPORunnerCfg(RslRlOnPolicyRunnerCfg):
+    num_steps_per_env = 24
+    max_iterations = 15000
+    save_interval = 500
+    experiment_name = "pf_flat"
+    empirical_normalization = False
+    # encoder_class_name = "MLP_Encoder"
+    # policy_class_name = "ActorCritic"
+    # algorithm_class_name = "PPO"
+    policy = RslRlPpoActorCriticCfg(
+        init_noise_std=1.0,
+        actor_hidden_dims=[512, 256, 128],
+        critic_hidden_dims=[512, 256, 128],
+        activation="elu",
+    )
+    algorithm = RslRlPpoAlgorithmCfg(
+        value_loss_coef=1.0,
+        use_clipped_value_loss=True,
+        clip_param=0.2,
+        entropy_coef=0.01,
+        num_learning_epochs=5,
+        num_mini_batches=4,
+        learning_rate=1.0e-3,
+        schedule="adaptive",
+        gamma=0.99,
+        lam=0.95,
+        desired_kl=0.01,
+        max_grad_norm=1.0, 
+    )
+    
+#-----------------------------------------------------------------
+@configclass
+class PF_TRON1AFlatPPORunnerCfg(RslRlOnPolicyRunnerCfg):
+    num_steps_per_env = 24
+    max_iterations = 2000
+    save_interval = 200
+    experiment_name = "pf_tron_1a_flat"
+    empirical_normalization = False
+    policy = RslRlPpoActorCriticCfg(
+        init_noise_std=1.0,
+        actor_hidden_dims=[512, 256, 128],
+        critic_hidden_dims=[512, 256, 128],
+        activation="elu",
+    )
+    algorithm = RslRlPpoAlgorithmMlpCfg(
+        class_name="PPO",
+        value_loss_coef=1.0,
+        use_clipped_value_loss=True,
+        clip_param=0.2,
+        entropy_coef=0.01,
+        num_learning_epochs=5,
+        num_mini_batches=4,
+        learning_rate=1.0e-3,
+        schedule="adaptive",
+        gamma=0.99,
+        lam=0.95,
+        desired_kl=0.01,
+        max_grad_norm=1.0,
+        obs_history_len=10,
+    )
+    encoder = EncoderCfg(
+        output_detach = True,
+        num_output_dim = 3,
+        hidden_dims = [256, 128],
+        activation = "elu",
+        orthogonal_init = False,
+    )
+
+#-----------------------------------------------------------------
+@configclass
+class SF_TRON1AFlatPPORunnerCfg(RslRlOnPolicyRunnerCfg):
+    num_steps_per_env = 24
+    max_iterations = 15000
+    save_interval = 500
+    experiment_name = "sf_tron_1a_flat"
+    empirical_normalization = False
+    policy = RslRlPpoActorCriticCfg(
+        init_noise_std=1.0,
+        actor_hidden_dims=[512, 256, 128],
+        critic_hidden_dims=[512, 256, 128],
+        activation="elu",
+    )
+    algorithm = RslRlPpoAlgorithmMlpCfg(
+        class_name="PPO",
+        value_loss_coef=1.0,
+        use_clipped_value_loss=True,
+        clip_param=0.2,
+        entropy_coef=0.01,
+        num_learning_epochs=5,
+        num_mini_batches=4,
+        learning_rate=1.0e-3,
+        schedule="adaptive",
+        gamma=0.99,
+        lam=0.95,
+        desired_kl=0.01,
+        max_grad_norm=1.0,
+        obs_history_len=10,
+    )
+    encoder = EncoderCfg(
+        output_detach = True,
+        num_output_dim = 3,
+        hidden_dims = [256, 128],
+        activation = "elu",
+        orthogonal_init = False,
+    )
+
+
+#-----------------------------------------------------------------
+@configclass
+class WF_TRON1AFlatPPORunnerCfg(RslRlOnPolicyRunnerCfg):
+    num_steps_per_env = 24
+    max_iterations = 10000
+    save_interval = 500
+    experiment_name = "wf_tron_1a_flat"
+    empirical_normalization = False
+    policy = RslRlPpoActorCriticCfg(
+        init_noise_std=1.0,
+        actor_hidden_dims=[512, 256, 128],
+        critic_hidden_dims=[512, 256, 128],
+        activation="elu",
+    )
+    algorithm = RslRlPpoAlgorithmMlpCfg(
+        class_name="PPO",
+        value_loss_coef=1.0,
+        use_clipped_value_loss=True,
+        clip_param=0.2,
+        entropy_coef=0.01,
+        num_learning_epochs=5,
+        num_mini_batches=4,
+        learning_rate=1.0e-3,
+        schedule="adaptive",
+        gamma=0.99,
+        lam=0.95,
+        desired_kl=0.01,
+        max_grad_norm=1.0,
+        obs_history_len=10,
+    )
+    encoder = EncoderCfg(
+        output_detach = True,
+        num_output_dim = 3,
+        hidden_dims = [256, 128],
+        activation = "elu",
+        orthogonal_init = False,
+    )
---
+++ tron_showroom/tron1/exts/bipedal_locomotion/bipedal_locomotion/utils/wrappers/rsl_rl/rl_mlp_cfg.py
@@ -0,0 +1,62 @@
+# Copyright (c) 2022-2024, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+from dataclasses import MISSING
+from typing import Literal
+
+from isaaclab.utils import configclass
+from isaaclab_rl.rsl_rl import RslRlPpoAlgorithmCfg
+
+
+@configclass
+class RslRlPpoAlgorithmMlpCfg(RslRlPpoAlgorithmCfg):
+    """Configuration of the runner for on-policy algorithms."""
+
+    # runner_type: str = "OnPolicyRunner"
+
+    obs_history_len: int = 1
+
+
+@configclass
+class EncoderCfg:
+    output_detach : bool = True
+    num_input_dim : int = MISSING
+    num_output_dim : int = 3
+    hidden_dims : list[int] = [256, 128]
+    activation : str = "elu"
+    orthogonal_init : bool = False
+
+
+import os
+import copy
+import torch
+def export_mlp_as_onnx(mlp, path, name, input_dim):
+    os.makedirs(path, exist_ok=True)
+    path = os.path.join(path, name + ".onnx")
+    model = copy.deepcopy(mlp).to("cpu")
+    model.eval()
+
+    dummy_input = torch.randn(input_dim)
+    input_names = ["mlp_input"]
+    output_names = ["mlp_output"]
+
+    torch.onnx.export(
+        model,
+        dummy_input,
+        path,
+        verbose=True,
+        input_names=input_names,
+        output_names=output_names,
+        export_params=True,
+        opset_version=13,
+    )
+    print("Exported policy as onnx script to: ", path)
+
+def export_policy_as_jit(actor_critic, path):
+    os.makedirs(path, exist_ok=True)
+    path = os.path.join(path, "policy.pt")
+    model = copy.deepcopy(actor_critic.actor).to("cpu")
+    traced_script_module = torch.jit.script(model)
+    traced_script_module.save(path)
